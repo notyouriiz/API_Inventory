3ac1788b0fa5356ef3d52536f62d539d8aa57738
RaffiDM <raffi.dzaky.mahendra@gmail.com>
2025-11-27 18:51:20 +0700

    update column position

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,6 @@ from extensions import db
 from datetime import datetime
 
 class TimestampMixin:
-    deleted_at = db.Column(db.DateTime, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
+    deleted_at = db.Column(db.DateTime, nullable=True)
