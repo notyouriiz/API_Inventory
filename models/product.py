26828f6aaace05470f436924cd337ab20431ebe3
RaffiDM <raffi.dzaky.mahendra@gmail.com>
2025-11-27 18:22:41 +0700

    inherit TimestampMixin

--- models/product.py
+++ models/product.py
@@ -8,8 +8,5 @@ class Product(TimestampMixin, db.Model):
     category_id = db.Column(db.BigInteger, db.ForeignKey("categories.id"), nullable=False)
     name = db.Column(db.String(150), nullable=False)
     stock = db.Column(db.Integer, default=0)
-    deleted_at = db.Column(db.DateTime, nullable=True)
-    created_at = db.Column(db.DateTime, default=datetime.now)
-    updated_at = db.Column(db.DateTime, default=datetime.now)
 
     category = db.relationship("Category", backref="products")    
