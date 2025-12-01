4a1de599e84c77af72be7c43012e81a790e690fc
catt.__ <22k10020@student.unika.ac.id>
2025-12-02 01:54:28 +0700

    update php files

--- models/category.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-from models.base_model import TimestampMixin
-
-class Category(TimestampMixin, db.Model):
-    __tablename__ = "categories"
-
-    id = db.Column(db.BigInteger, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
