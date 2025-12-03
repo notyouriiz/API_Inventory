857106b18f39ae63b19aa994fec466760192ffaa
RaffiDM <raffi.dzaky.mahendra@gmail.com>
2025-12-03 20:39:37 +0700

    Enable API access from other devices in local network

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ if __name__ == "__main__":
         db.create_all()
         print("Database tables created successfully!")
         
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
